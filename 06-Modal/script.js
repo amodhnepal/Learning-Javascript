0d2cef17c7cff0e39d418297c56c1dfc1a98f483
amodhnepal <nepalamodh8@gmail.com>
2024-05-14 19:39:05 +0545

    Handling key press events

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -6,7 +6,7 @@ const btnsOpenModal=document.querySelectorAll('.show-modal');
 
 
 const openModal=function(){
-    console.log("button clicked");
+
         modal.classList.remove('hidden');
         overlay.classList.remove('hidden');
 }
@@ -20,3 +20,13 @@ for(let i=0;i<btnsOpenModal.length;i++)
 
     btnCloseModal.addEventListener('click',closeModal);
     overlay.addEventListener('click',closeModal);
+
+    document.addEventListener('keydown',function(e){
+        console.log('A key was pressed');
+        console.log(e.key);
+        if(e.key==='Escape' && !modal.classList.contains('hidden')) {
+                closeModal();
+
+        }
+
+    });
