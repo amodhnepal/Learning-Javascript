31e16dd612f574a4401526cb14536c1eef04da9a
amodhnepal <nepalamodh8@gmail.com>
2024-05-27 08:32:13 +0545

    Sets and Maps

--- 08-Data-Structures-Operators/starter/script.js
+++ 08-Data-Structures-Operators/starter/script.js
@@ -38,32 +38,57 @@ const restaurant = {
     console.log(`Here os your delicious pasta with ${ing1},${ing2},${ing3}`);
   }
 };
- const orderSet=new Set([
-  'Pasta',
-  'Pasta',
-  'Pasta',
-  'Pizza',
-  'Risotto',
- ]);
- console.log(orderSet);
- console.log(new Set('jonas'));
- console.log(orderSet.size);
- console.log(orderSet.has('Bread'));
-orderSet.add('Garlic Bread');
-orderSet.add('Garlic Bread');
-orderSet.delete('Risotto');
-// orderSet.clear();
-console.log(orderSet);
+
+// MAps
+const rest =new Map();
+rest.set('name','Classico Italiano');
+rest.set(1,'Firenze,Italy');
+console.log(rest.set(2,'Lisbon,Portugal'));
+
+rest.set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+.set('open',11)
+.set('close',23)
+.set(true,'We are open :D')
+.set(false,'We are closed :(');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time=21;
+console.log( rest.get(time>rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest);
+console.log(rest.size);
+// rest.clear();
 
 
 
-for(const order of orderSet) console.log(order);
+//  const orderSet=new Set([
+//   'Pasta',
+//   'Pasta',
+//   'Pasta',
+//   'Pizza',
+//   'Risotto',
+//  ]);
+//  console.log(orderSet);
+//  console.log(new Set('jonas'));
+//  console.log(orderSet.size);
+//  console.log(orderSet.has('Bread'));
+// orderSet.add('Garlic Bread');
+// orderSet.add('Garlic Bread');
+// orderSet.delete('Risotto');
+// // orderSet.clear();
+// console.log(orderSet);
 
-// Example
-const staff=['Waiter', 'Chef', 'Waiter', 'Manager'];
-const staffUnique= [...new Set(staff)];
-console.log(staffUnique);
-console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager']));
+
+
+// for(const order of orderSet) console.log(order);
+
+// // Example
+// const staff=['Waiter', 'Chef', 'Waiter', 'Manager'];
+// const staffUnique= [...new Set(staff)];
+// console.log(staffUnique);
+// console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager']));
 // const arr=[1,2,...[3,4]];
 // const [a,b,...others]=[1,2,3,4,5];
 // console.log(a,b,others);
